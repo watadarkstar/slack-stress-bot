4d064879e6868e4bf6f4f54149f5b062fcea57f8
Adrian Carolli <adrian.carolli1@gmail.com>
2019-04-22 23:07:05 -0400

    Got the event message adapter to work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { WebClient } = require("@slack/web-api");
+const { createEventAdapter } = require("@slack/events-api");
 
 // Creates express app
 const app = express();
@@ -14,15 +15,31 @@ app.listen(process.env.PORT || PORT, function() {
 
 // Read a token from the environment variables
 const token = process.env.SLACK_AUTH_TOKEN;
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 
 // Initialize
 const web = new WebClient(token);
+const slackEvents = createEventAdapter(slackSigningSecret);
 
 // channel
 const channel = "#stress-checkin";
 
+// Plug the adapter in as a middleware
+app.use("/slack/events", slackEvents.requestListener());
+
+// Example: If you're using a body parser, always put it after the event adapter in the middleware stack
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on("message", event => {
+  console.log(
+    `Received a message event: user ${event.user} in channel ${
+      event.channel
+    } says ${event.text}`
+  );
+});
+
 app.post("/", async (req, res) => {
   const message1 = {
     text: `Hi, I hope you are having a great day! :sunny:\n\nAs part of a wellness program we are running, each week your manager wants me to check-in with you regarding your stress levels at work.\n\n*How are you feeling this week on a scale of 0 - 10 (0 means no stress and 10 is a lot stress)?*`,
